24fd09e751bcd4be4a7312021e74f6b56c81dbdd
eliott <backup2@eliottdesk>
2025-05-05 08:17:48 -0600

    little more

--- cmd/tsnet-cni/main.go
+++ cmd/tsnet-cni/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
-
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/version"
 	"github.com/containernetworking/cni/pkg/types"
@@ -10,8 +13,34 @@ import (
 	"github.com/eliottcassidy2000/monad-forwarder/internal/daemon_client"
 )
 
-func cmdAdd(args *skel.CmdArgs) error {
+type NetConf struct {
+    CNIVersion string `json:"cniVersion"`
+    Name       string `json:"name"`
+    Type       string `json:"type"`
+}
 
+func parseCNIArgs() (map[string]string) {
+    args := make(map[string]string)
+    raw := os.Getenv("CNI_ARGS")
+    if raw == "" {
+        return args
+    }
+    for _, pair := range strings.Split(raw, ";") {
+        kv := strings.SplitN(pair, "=", 2)
+        if len(kv) == 2 {
+            args[kv[0]] = kv[1]
+        }
+    }
+    return args
+}
+
+
+func cmdAdd(args *skel.CmdArgs) error {
+	conf := &NetConf{}
+    if err := json.Unmarshal(args.StdinData, conf); err != nil {
+        return err
+    }
+	fmt.Println(conf.CNIVersion)
 	// Connect to daemon
 	client, err := daemon_client.NewClient("/var/run/tsnet-daemon.sock")
 	if err != nil {
@@ -24,41 +53,9 @@ func cmdAdd(args *skel.CmdArgs) error {
 	if err != nil {
 		return err
 	}
-	// ipcfg := &types.IPConfig{
-	// 	Version: "4",
-	// 	Address: *ip,
-	// 	Gateway: nil,
-	// }
 	res := &cniTypesV1.IPConfig{Address: *ip}
 	res2 := &cniTypesV1.Result{IPs: []*cniTypesV1.IPConfig{res}}
-	return types.PrintResult(res2, "1.0.0")
-	// return types.PrintResult(&types.Result{
-	// 	Version: types.Current(),
-	// 	IPs: []*types.IPConfig{{
-	// 		Version: "4",
-	// 		Address: *ip,
-	// 		Gateway: nil,
-	// 	}},
-	// }, types.Current())			
-	// Build CNI result
-	// result := &types.Result{
-	// 	Version: types.Current(),
-	// 	IPs: []*types.IPConfig{{
-	// 		Version: "4",
-	// 		Address: *ip,
-	// 		Gateway: nil,
-	// 	}},
-	// }
-	// return result.Print()
-	// result := &current.Result{
-	// 	CNIVersion: types.Current(),
-	// 	IPs: []*current.IPConfig{{
-	// 		Version: "4",
-	// 		Address: *ip,
-	// 		Gateway: nil,
-	// 	}},
-	// }
-	// return result.Print()
+	return types.PrintResult(res2, conf.CNIVersion)
 }
 
 func cmdDel(args *skel.CmdArgs) error {
@@ -71,8 +68,8 @@ func cmdDel(args *skel.CmdArgs) error {
 }
 
 func main() {
-	skel.PluginMainFuncs(skel.CNIFuncs{Add:  cmdAdd,
-Del: cmdDel, Check: cmdCheck}, version.PluginSupports("0.1.0", "0.2.0", "0.3.0", "0.3.1", "0.4.0", "1.0.0", "1.1.0"),"tsnet-cni")
+	skel.PluginMainFuncs(skel.CNIFuncs{Add:  cmdAdd, Del: cmdDel, Check: cmdCheck}, version.PluginSupports("0.1.0", "0.2.0", "0.3.0", "0.3.1", "0.4.0", "1.0.0", "1.1.0"),"tsnet-cni")
+	//skel.PluginMainFuncs(skel.CNIFuncs{Add:  cmdAdd, Del: cmdDel, Check: cmdCheck}, version.PluginSupports("1.0.0"),"tsnet-cni")
 }
 
 func cmdCheck(args *skel.CmdArgs) error { return nil }
